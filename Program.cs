44424170e24f92803fe689a37b21ae1c495e6fbf
Alec Allain <allainal@mail.gvsu.edu>
2020-03-13 15:50:34 -0400

    Added switch case; More UI dialog

--- Program.cs
+++ Program.cs
@@ -22,13 +22,44 @@ namespace MortgageCalculator
         private int amount {get; set;}
         private int years {get; set;}
         private double rate {get; set;}
+        private static int test = 0;
+        private static int choice = 0;
+        private static bool result = true;
 
         /**
         * Main method
         */
         static void Main(string[] args)
         {
+            // Introduction
             Console.WriteLine("Hello World!");
+            Console.WriteLine("This program is a test to show my introductory knowledge to the C#");
+
+            Calculator calc = new Calculator();
+
+            Console.WriteLine("What is your name? Enter it below: ");
+            while (int.TryParse(Console.ReadLine(), out test)) {
+                Console.WriteLine("Please do not have any numbers in your name");
+                Console.WriteLine("What is your name? Enter it below: ");
+            }
+
+            Console.WriteLine("Hello {0}! We are now going to calculate your mortgage\nWould you like to calculate the monthly cost of your mortgage\nor would you like to calculate the maximum amount of money you can take out for a loan?", calc.name);
+            switch(choice)
+            {
+                case 1: 
+                    Console.WriteLine("You have chosen to calcluate your monthly cost");
+                    break;
+                case 2:
+                    Console.WriteLine("You have chosen to calculate your maximum amount");
+                    break;
+                case 3:
+                    Console.WriteLine("You have chosen to exit the program\nHave a great day!");
+                    break;
+                default:
+                    Console.WriteLine("I'm sorry, please choose a valid option");
+                    break;
+            }
+
         }
     
         /**

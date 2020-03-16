ff61a97ce95ef255ef18ebbec5eceaf66a64209f
Alec Allain <allainal@mail.gvsu.edu>
2020-03-16 14:29:35 -0400

    Improved data entry specifications; Added looping

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Diagnostics;
 using System.Linq;
+//using System.Windows.Forms;
 
 /*****************************************
 * This class simulates a simple mortgage 
@@ -38,14 +39,21 @@ namespace MortgageCalculator
             Calculator calc = new Calculator();
 
             Console.WriteLine("What is your name? Enter it below: ");
-            while (int.TryParse(Console.ReadLine(), out test)) {
-                Console.WriteLine("Please do not have any numbers in your name");
+            calc.name = Console.ReadLine();
+
+            while (int.TryParse(calc.name, out test)) {
+                Console.WriteLine("Please do not have any numbers or special characters in your name");
                 Console.WriteLine("What is your name? Enter it below: ");
+                calc.name = Console.ReadLine();
             }
 
-            Console.WriteLine("Hello {0}! We are now going to calculate your mortgage\nWould you like to calculate the monthly cost of your mortgage\nor would you like to calculate the maximum amount of money you can take out for a loan?", calc.name);
-            switch(choice)
-            {
+            Console.WriteLine("Hello {0}! We are now going to calculate your mortgage. Would you like to:\n1. Calculate the monthly cost of your mortgage\n2. Calculate the maximum amount of money you can take out for a loan\n3. Exit the program", calc.name);
+            Console.WriteLine("Enter the number of the choice you pick: ");
+
+            choice = Convert.ToInt32(Console.ReadLine());
+            while (choice != 3) {
+                switch(choice)
+                {
                 case 1: 
                     Console.WriteLine("You have chosen to calcluate your monthly cost");
                     break;
@@ -58,8 +66,13 @@ namespace MortgageCalculator
                 default:
                     Console.WriteLine("I'm sorry, please choose a valid option");
                     break;
+                }
+                Console.WriteLine("Would you like to:\n1. Calculate the monthly cost of your mortgage\n2. Calculate the maximum amount of money you can take out for a loan\n3. Exit the program");
+                Console.WriteLine("Enter the number of the choice you pick: ");
+                choice = Convert.ToInt32(Console.ReadLine());
             }
-
+            Console.WriteLine("You have chosen to exit the program\nHave a great day!");
+            Console.WriteLine("Look out for more future .NET Framework and C# applications from me!");
         }
     
         /**
